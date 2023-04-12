31cd73404adfa55c8de11cebab4871cb57610c45
Per Næss <perness3009@gmail.com>
2023-04-12 21:12:07 +0200

    feat: add tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::{framework::{StandardFramework, standard::{macros::{group, command}, CommandResult}}, prelude::{Context, GatewayIntents, EventHandler}, model::prelude::Message, Client, async_trait};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~"))
+        .group(&GENERAL_GROUP);
+
+    // Login with token
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // Start listening for events
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why)
+    }
+
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
 }
