7f2f74d8c3c6eebb6f1f95bced7ad1eaeaa81af8
Per Næss <perness3009@gmail.com>
2023-07-18 18:52:54 +0200

    feat: add eventhandler

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use serenity::prelude::GatewayIntents;
+use std::env;
+
+use serenity::{
+    async_trait,
+    model::prelude::Ready,
+    prelude::{Context, EventHandler, GatewayIntents},
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected", ready.user.name)
+    }
+}
 
 fn main() {
+    let token = env::var("DISCORD_TOKEN").expect("Expected a discord token in the environment");
+
     let intents = GatewayIntents::GUILDS
         | GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::GUILD_VOICE_STATES;
